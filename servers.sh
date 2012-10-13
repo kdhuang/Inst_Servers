dacf24360c25812b67416fe61fdd3ddf8bbcb3d5
kdhuang <k.d.huang13@gmail.com>
2012-10-12 23:57:59 -0700

    Basic functionality of telling you which server is up is running is functional

--- servers.sh
+++ servers.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "Username:"
+servers=( delicious.cs.berkeley.edu barry.cs.berkeley.edu )
+
+for server in "${servers[@]}"; do
+	ping=`ping -c 2 $server`
+	if [ -n "$ping" ]; then
+		echo -e "$server \t Status: Running"
+	else
+		echo -e "$server \t Status: Not Running"
+	fi
+done
